abffe12de7f8670ccdf2af9cfb87e429439c3e4b
NERMEEN ASHRAF <nnerrmmeenn@gmail.com>
2024-10-02 15:57:05 +0300

    Create Entities

---
+++ HMSphere.Domain/Entities/DoctorShift.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMSphere.Domain.Entities
+{
+    internal class DoctorShift
+    {
+        [Required]
+        public int Id { get; set; }
+
+        // Foreign Key for class Doctor
+        [Required]
+        public int DoctorId { get; set; }
+
+        // Foreign Key for class Shift
+        [Required]
+        public int ShiftId { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ HMSphere.Domain/Entities/Staff.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMSphere.Domain.Entities
+{
+    internal class Staff : ApplicationUser
+    {
+        //[Required]
+        //[StringLength(50)]
+        public string Role { get; set; }
+
+
+        //[Required]
+        //[StringLength(100)]
+        public string JobTitle { get; set; }
+
+
+        //[Required]
+        //[DataType(DataType.Date)]
+        public DateTime HireDate { get; set; }
+
+        // Foreign Key for DeptId
+        //[Required]
+        public int DeptId { get; set; }
+
+
+        // Foreign Key for Shift 
+        //[Required]
+        public int ShiftId { get; set; }
+    }
+}
