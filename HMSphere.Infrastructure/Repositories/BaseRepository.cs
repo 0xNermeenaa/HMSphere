96309f6cd1dcc6cd0da273aae15d13b85ab1f68b
Ahmed Nasser <an229314@gmail.com>
2024-10-07 17:20:58 +0200

    nasser

--- HMSphere.Infrastructure/Repositories/BaseRepository.cs
+++ HMSphere.Infrastructure/Repositories/BaseRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HMSphere.Infrastructure.DataContext;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,44 @@ using System.Threading.Tasks;
 
 namespace HMSphere.Infrastructure.Repositories
 {
-    public class BaseRepository: IBaseRepository
+    public class BaseRepository<T>: IBaseRepository<T> where T : class
     {
+        private readonly HmsContext _context;
+        private readonly DbSet<T> _dbSet;
 
+        public BaseRepository(HmsContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- HMSphere.Infrastructure/Repositories/IBaseRepository.cs
+++ HMSphere.Infrastructure/Repositories/IBaseRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace HMSphere.Infrastructure.Repositories
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<T> where T : class
     {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
