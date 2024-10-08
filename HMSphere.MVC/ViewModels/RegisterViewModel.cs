73670ca3dbc5b09d75ec98b6ae6c3bafd54ca426
NERMEEN ASHRAF <nnerrmmeenn@gmail.com>
2024-10-08 06:30:59 +0300

    Registration page

--- HMSphere.MVC/ViewModels/RegisterViewModel.cs
+++ HMSphere.MVC/ViewModels/RegisterViewModel.cs
@@ -4,13 +4,20 @@ namespace HMSphere.MVC.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string Username { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
