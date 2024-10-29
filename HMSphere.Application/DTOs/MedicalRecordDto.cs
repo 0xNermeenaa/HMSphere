85a7ab5d721567d8dc9fdabf1252e9ea80321afd
Nermeen <nnerrmmeenn@gmail.com>
2024-10-29 16:05:00 +0300

    يااارب نخلص

--- HMSphere.Application/DTOs/MedicalRecordDto.cs
+++ HMSphere.Application/DTOs/MedicalRecordDto.cs
@@ -2,12 +2,15 @@
 {
     public class MedicalRecordDto
     {
-		public string PatientId { get; set; }
-		public DateTime CreatedDate { get; set; }
-        public string Diagnosis { get; set; }
+        public int? Id { get; set; }
+		public string? PatientId { get; set; }
+        public string? DoctorId { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public string? Diagnosis { get; set; }
         public string? TreatmentPlan { get; set; }
-        public string Medications { get; set; }
+        public string? Medications { get; set; }
         public string? DoctorNotes { get; set; }
-        public DateTime LastUpdated { get; set; }
+        public DateTime? LastUpdated { get; set; }
     }
 }
--- HMSphere.Infrastructure/DataContext/StoredContextSeed.cs
+++ HMSphere.Infrastructure/DataContext/StoredContextSeed.cs
@@ -15,16 +15,16 @@ namespace HMSphere.Infrastructure.DataContext
     {
         public static async Task SeedAsync(HmsContext context)
         {
-            //if (!context.Departments.Any())
-            //{
-            //    var departmentsData = File.ReadAllText("../HMSphere.Infrastructure/SeedData/Departments.json");
-            //    var departments = JsonSerializer.Deserialize<List<Department>>(departmentsData);
-            //    foreach (var item in departments)
-            //    {
-            //        context.Departments.Add(item);
-            //    }
-            //    await context.SaveChangesAsync();
-            //}
+            if (!context.Departments.Any())
+            {
+                var departmentsData = File.ReadAllText("../HMSphere.Infrastructure/SeedData/Departments.json");
+                var departments = JsonSerializer.Deserialize<List<Department>>(departmentsData);
+                foreach (var item in departments)
+                {
+                    context.Departments.Add(item);
+                }
+                await context.SaveChangesAsync();
+            }
         }
 
         public static async Task AppointmentSeed(HmsContext context)
